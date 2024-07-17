4c6bc8b78f4b609768259ee89174c634130e5502
hyunseoko <kohyun0223@skuniv.ac.kr>
2024-07-17 17:36:11 +0900

    First commit

---
+++ src/main/java/hanium/smath/SmathApplication.java
@@ -0,0 +1,13 @@
+package hanium.smath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmathApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hanium/smath/SmathApplicationTests.java
@@ -0,0 +1,13 @@
+package hanium.smath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
