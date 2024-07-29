b311d34d3352cfeaf635c4fc288e48044d26e9b0
hyunseoko <kohyuin0223@skuniv.ac.kr>
2024-07-29 18:01:17 +0900

    Logout(설정페이지)

---
+++ src/main/java/hanium/smath/Member/controller/LogoutController.java
@@ -0,0 +1,24 @@
+package hanium.smath.Member.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/api/members")
+public class LogoutController {
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logoutUser(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return ResponseEntity.ok("Logout successful");
+    }
+}
