8eebaaa702612af342640b729f820bc7e4612f40
chaeelin <chaeee0123@gmail.com>
2024-09-16 22:53:30 +0900

    Notice - first commit

---
+++ src/main/java/hanium/smath/Notice/Repository/NoticeRepository.java
@@ -0,0 +1,8 @@
+package hanium.smath.Notice.Repository;
+
+import hanium.smath.Notice.entity.Notice; // 수정됨
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// 나머지 코드는 동일
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
